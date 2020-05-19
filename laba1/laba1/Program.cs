36bb6867528d09295d57368e55c7aae30fa30722
bumb217 <bumb2172@gmail.com>
2020-05-19 21:20:23 +0300

    done

---
+++ laba1/laba1/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fat_dialog
+{
+    class Program
+    {
+        public static string[] answers =
+    {
+                "Dank as duck",
+                "WeSmart",
+                "MonkaHmm",
+                "LUL",
+                "BibleThump",
+                "KEKWait",
+                "BlessRNG",
+                "CoolStoryBob",
+                "Kappa",
+                "Jebaited",
+                "SMOrc",
+            };
+        public static string[] questions =
+          {
+                "Was ist das?",
+                "Do you know de way?",
+                "Where you've been to?",
+                "If life is unfair to everyone, does that make it fair?",
+                "Do you really think so?",
+                "What do you think about that?",
+                "If we can't see air, do fish see water?",
+                "Is it true?",
+                "Who's Elon Musk?",
+                "Ein Volk, Ein... zwei, Polizei?",
+                "Pizza time",
+            };
+        public static string[] generalAnswer = { "Yes, I ", "Nope, I not " };
+        public static string[] time = { "months", "years", "weeks" };
+        public static string[] whyanswers = {
+            "Cosmic fluke.",
+            "Eror 404.",
+            "Because.",
+            "wat?"
+        };
+
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            Console.WriteLine("Master Yoda: May the force be with you");
+            string result = "";
+            while (true)
+            {
+                result = "BabyYoda: ";
+                string s = Console.ReadLine();
+                string[] words = s.Split(' ');
+                if (string.IsNullOrWhiteSpace(s))
+                    Console.WriteLine("BabyYoda: What`s new?");
+                else
+                {
+                    if (s.Contains("?"))
+                    {
+                        if (s.Contains("Do") || s.Contains("Did") || s.Contains("Will") || s.Contains("Have"))
+                        {
+                            result += generalAnswer[r.Next(generalAnswer.Length)];
+                            for (int i = 2; i < words.Length; i++)
+                                result += words[i].Replace('?', '.') + " ";
+                            result += "And you?";
+
+                        }
+                        else if (s.Contains("Why"))
+                            result += whyanswers[r.Next(whyanswers.Length)];
+                        else if (s.Contains("How long") || s.Contains("How often"))
+                            result += "Hmmmm. Near " + r.Next(10) + " " + time[r.Next(time.Length)];
+                        else if (s.Contains("How"))
+                            result += "O, I`m perfect.";
+                        else result = answers[r.Next(answers.Length)];
+                    }
+                    else result += questions[r.Next(questions.Length)];
+                    Console.WriteLine(result);
+                }
+            }
+        }
+    }
+}
---
+++ laba1/laba1/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("laba1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("laba1")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("66abe0c1-0999-4660-b7cf-3a012c4eea06")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
